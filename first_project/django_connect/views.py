8c3778ecefb25a07f8c57519a17c57af6c85d1af
root <root@chop-rh8-01.jnrit.com>
2023-05-19 23:43:51 +0530

    added views

--- first_project/django_connect/views.py
+++ first_project/django_connect/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    my_dict = {'insert_content':"Hello I am from django_connect"}
+    return render(request,'django_connect/index.html',context=my_dict)
