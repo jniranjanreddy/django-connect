d35676aa5895d9cf35ca8ad8cafa3f0329d5dda0
root <root@chop-rh8-01.jnrit.com>
2023-05-24 14:06:26 +0530

    ad

--- first_project/django_connect/admin.py
+++ first_project/django_connect/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# from django_connect.models import AccessRecord,Topic,Webpage
+# # Register your models here.
+# admin.site.register(AccessRecord)
+# admin.site.register(Topic)
+# admin.site.register(Webpage)
