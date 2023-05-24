23dc3810b5febcaa1a2f411c9236c488c7baec24
root <root@chop-rh8-01.jnrit.com>
2023-05-24 14:27:54 +0530

    added

--- first_project/django_connect/admin.py
+++ first_project/django_connect/admin.py
@@ -1,6 +1,8 @@
-# from django.contrib import admin
-# from django_connect.models import AccessRecord,Topic,Webpage
-# # Register your models here.
-# admin.site.register(AccessRecord)
-# admin.site.register(Topic)
-# admin.site.register(Webpage)
+from django.contrib import admin
+#from django_connect.models import AccessRecord,Topic,Webpage
+from django_connect.models import Topic
+
+# Register your models here.
+#admin.site.register(AccessRecord)
+admin.site.register(Topic)
+#admin.site.register(Webpage)
--- first_project/django_connect/models.py
+++ first_project/django_connect/models.py
@@ -1,10 +1,10 @@
-# from django.db import models
+from django.db import models
 
-# class Topic(models.Model):
-# 	top_name = models.CharField(max_length=264,unique=True)
+class Topic(models.Model):
+	top_name = models.CharField(max_length=264,unique=True)
     
-# 	def __str__(self):
-# 		return self.top_name
+	def __str__(self):
+		return self.top_name
 
 # class Webpage(models.Model):
 # 	topic = models.ForeignKey(Topic)
