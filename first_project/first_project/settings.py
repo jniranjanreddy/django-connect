630a31699345cafb72f32f293c6d441f3609274d
root <root@chop-rh8-01.jnrit.com>
2023-05-24 17:15:59 +0530

    ad

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6=ha0p_e4wta%(_+4k353hirig9m@tb4bbry)@nw!lgfyjk+j^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
